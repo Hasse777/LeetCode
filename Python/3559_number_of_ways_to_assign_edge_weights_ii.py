5fdef4b95526a7c602531c271f323be728953d80
Hasse <Dima8819@yandex.ru>
2026-06-13 05:00:53 +0300

    Оптимизировал решение задачи 3559 с помощью алгоритма двоичного подъёма. Теперь все тесты проходят. Пора спать

--- Python/3559_number_of_ways_to_assign_edge_weights_ii.py
+++ Python/3559_number_of_ways_to_assign_edge_weights_ii.py
@@ -16,12 +16,13 @@ class Solution(object):
             graph[j].append(i)
 
         result = list()
-        parent = dict()
         depth_node = dict()
         stack = list()
         visited = set()
         stack.append((1, 0))
-        parent[1] = -1
+        n = len(edges) + 1
+        jump = len(edges).bit_length()
+        up = [[-1] * jump for _ in range(n + 1)]
 
         while stack:
             node, depth = stack.pop()
@@ -32,25 +33,40 @@ class Solution(object):
             for neighbor in graph[node]:
                 if neighbor not in visited:
                     stack.append((neighbor, depth + 1))
-                    parent[neighbor] = node
+                    up[neighbor][0] = node
 
-        for query in queries:
-            node1, node2 = query
-            lca = self.lca(node1, node2, parent, depth_node)
-            distance = depth_node[node1] + depth_node[node2] - 2 * depth_node[lca]
+        for j in range(1, jump):
+            for i in range(1, n + 1):
+                if up[i][j - 1] != -1:
+                    up[i][j] = up[up[i][j - 1]][j - 1]
+
+        for a, b in queries:
+            p = self.lca(a, b, depth_node, up, jump)
+            distance = depth_node[a] + depth_node[b] - 2 * depth_node[p]
+
             if distance == 0:
                 result.append(0)
-                continue
-
-            result.append(pow(2, distance - 1, 10**9 + 7))
+            else:
+                result.append(pow(2, distance - 1, 10**9 + 7))
         return result
 
-    def lca(self, node1, node2, parent, depth_node):
-        while depth_node[node1] > depth_node[node2]:
-            node1 = parent[node1]
-        while depth_node[node2] > depth_node[node1]:
-            node2 = parent[node2]
-        while node1 != node2:
-            node1 = parent[node1]
-            node2 = parent[node2]
-        return node1
+    def lca(self, node1, node2, depth_node, up, jump):
+        if depth_node[node1] < depth_node[node2]:
+            node1, node2 = node2, node1
+        dif = depth_node[node1] - depth_node[node2]
+
+        i = 0
+        while dif:
+            if dif & 1:
+                node1 = up[node1][i]
+            i += 1
+            dif >>= 1
+
+        if node1 == node2:
+            return node1
+
+        for i in range(jump - 1, -1, -1):
+            if up[node1][i] != up[node2][i]:
+                node1 = up[node1][i]
+                node2 = up[node2][i]
+        return up[node1][0]
